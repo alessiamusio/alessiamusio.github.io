9d44c2d7675ca134431020c12da087d53c526972
Alessia Musio <alessiamusio.am@gmail.com>
2023-02-21 17:53:36 +0100

    site

--- js/main.js
+++ js/main.js
@@ -26,22 +26,22 @@ btn.addEventListener("click", function () {
 
 //TOOLTIP
 let screenLog = document.querySelector('#screen-log');
-        document.addEventListener('mousemove', logKey);
+document.addEventListener('mousemove', logKey);
 
 function logKey(e) {
-    placeDiv(e.screenX, e.screenY);
-    placeDiv(e.clientX, e.clientY);
+  placeDiv(e.screenX, e.screenY);
+  placeDiv(e.clientX, e.clientY);
 }
-        
- function placeDiv(x_pos, y_pos) {
+
+function placeDiv(x_pos, y_pos) {
   var d = document.getElementById('TextHidden');
   d.style.position = "absolute";
-  d.style.left = x_pos+'px';
-  d.style.top = y_pos+'px';
+  d.style.left = x_pos + 'px';
+  d.style.top = y_pos + 'px';
 }
 
 
-  //NOISE
+//NOISE
 const noise1 = () => {
   let canvas, ctx;
 
@@ -131,91 +131,90 @@ noise1();
 
 
 
-  //NOISE
-  const noise2 = () => {
-    let canvas, ctx;
-  
-    let wWidth, wHeight;
-  
-    let noiseData = [];
-    let frame = 0;
-  
-    let loopTimeout;
-  
-    // Create Noise
-    const createNoise = () => {
-      const idata = ctx.createImageData(wWidth, wHeight);
-      const buffer32 = new Uint32Array(idata.data.buffer);
-      const len = buffer32.length;
-  
-      for (let i = 0; i < len; i++) {
-        if (Math.random() < 0.5) {
-          buffer32[i] = 0xffff0000;
-        }
+//NOISE
+const noise2 = () => {
+  let canvas, ctx;
+
+  let wWidth, wHeight;
+
+  let noiseData = [];
+  let frame = 0;
+
+  let loopTimeout;
+
+  // Create Noise
+  const createNoise = () => {
+    const idata = ctx.createImageData(wWidth, wHeight);
+    const buffer32 = new Uint32Array(idata.data.buffer);
+    const len = buffer32.length;
+
+    for (let i = 0; i < len; i++) {
+      if (Math.random() < 0.5) {
+        buffer32[i] = 0xffff0000;
       }
-  
-      noiseData.push(idata);
-    };
-  
-    // Play Noise
-    const paintNoise = () => {
-      if (frame === 5) {
-        frame = 0;
-      } else {
-        frame++;
-      }
-  
-      ctx.putImageData(noiseData[frame], 0, 0);
-    };
-  
-    // Loop
-    const loop = () => {
-      paintNoise(frame);
-  
-      loopTimeout = window.setTimeout(() => {
-        window.requestAnimationFrame(loop);
-      }, 1000 / 25);
-    };
-  
-    // Setup
-    const setup = () => {
-      wWidth = window.innerWidth;
-      wHeight = document.body.scrollHeight;
-      canvas.width = wWidth;
-      canvas.height = wHeight;
-  
-      for (let i = 0; i < 10; i++) {
-        createNoise();
-      }
-  
-      loop();
-    };
-  
-    // Reset
-    let resizeThrottle;
-    const reset = () => {
-      window.addEventListener(
-        "resize",
-        () => {
-          window.clearTimeout(resizeThrottle);
-  
-          resizeThrottle = window.setTimeout(() => {
-            window.clearTimeout(loopTimeout);
-            setup();
-          }, 200);
-        },
-        false
-      );
-    };
-  
-    // Init
-    const init = (() => {
-      canvas = document.getElementById("noise2");
-      ctx = canvas.getContext("2d");
-  
-      setup();
-    })();
+    }
+
+    noiseData.push(idata);
   };
-  
-  noise2();
-  
+
+  // Play Noise
+  const paintNoise = () => {
+    if (frame === 5) {
+      frame = 0;
+    } else {
+      frame++;
+    }
+
+    ctx.putImageData(noiseData[frame], 0, 0);
+  };
+
+  // Loop
+  const loop = () => {
+    paintNoise(frame);
+
+    loopTimeout = window.setTimeout(() => {
+      window.requestAnimationFrame(loop);
+    }, 1000 / 25);
+  };
+
+  // Setup
+  const setup = () => {
+    wWidth = window.innerWidth;
+    wHeight = document.body.scrollHeight;
+    canvas.width = wWidth;
+    canvas.height = wHeight;
+
+    for (let i = 0; i < 10; i++) {
+      createNoise();
+    }
+
+    loop();
+  };
+
+  // Reset
+  let resizeThrottle;
+  const reset = () => {
+    window.addEventListener(
+      "resize",
+      () => {
+        window.clearTimeout(resizeThrottle);
+
+        resizeThrottle = window.setTimeout(() => {
+          window.clearTimeout(loopTimeout);
+          setup();
+        }, 200);
+      },
+      false
+    );
+  };
+
+  // Init
+  const init = (() => {
+    canvas = document.getElementById("noise2");
+    ctx = canvas.getContext("2d");
+
+    setup();
+  })();
+};
+
+noise2();
