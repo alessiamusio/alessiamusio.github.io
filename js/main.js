3756aa45dc508c758ed3b26351c3e9ec4cffb6fd
Alessia Musio <alessiamusio.am@gmail.com>
2023-02-21 17:41:41 +0100

    ale

--- js/main.js
+++ js/main.js
@@ -39,3 +39,183 @@ function logKey(e) {
   d.style.left = x_pos+'px';
   d.style.top = y_pos+'px';
 }
+
+
+  //NOISE
+const noise1 = () => {
+  let canvas, ctx;
+
+  let wWidth, wHeight;
+
+  let noiseData = [];
+  let frame = 0;
+
+  let loopTimeout;
+
+  // Create Noise
+  const createNoise = () => {
+    const idata = ctx.createImageData(wWidth, wHeight);
+    const buffer32 = new Uint32Array(idata.data.buffer);
+    const len = buffer32.length;
+
+    for (let i = 0; i < len; i++) {
+      if (Math.random() < 0.5) {
+        buffer32[i] = 0xffff0000;
+      }
+    }
+
+    noiseData.push(idata);
+  };
+
+  // Play Noise
+  const paintNoise = () => {
+    if (frame === 5) {
+      frame = 0;
+    } else {
+      frame++;
+    }
+
+    ctx.putImageData(noiseData[frame], 0, 0);
+  };
+
+  // Loop
+  const loop = () => {
+    paintNoise(frame);
+
+    loopTimeout = window.setTimeout(() => {
+      window.requestAnimationFrame(loop);
+    }, 1000 / 25);
+  };
+
+  // Setup
+  const setup = () => {
+    wWidth = window.innerWidth;
+    wHeight = document.body.scrollHeight;
+    canvas.width = wWidth;
+    canvas.height = wHeight;
+
+    for (let i = 0; i < 10; i++) {
+      createNoise();
+    }
+
+    loop();
+  };
+
+  // Reset
+  let resizeThrottle;
+  const reset = () => {
+    window.addEventListener(
+      "resize",
+      () => {
+        window.clearTimeout(resizeThrottle);
+
+        resizeThrottle = window.setTimeout(() => {
+          window.clearTimeout(loopTimeout);
+          setup();
+        }, 200);
+      },
+      false
+    );
+  };
+
+  // Init
+  const init = (() => {
+    canvas = document.getElementById("noise1");
+    ctx = canvas.getContext("2d");
+
+    setup();
+  })();
+};
+
+noise1();
+
+
+
+  //NOISE
+  const noise2 = () => {
+    let canvas, ctx;
+  
+    let wWidth, wHeight;
+  
+    let noiseData = [];
+    let frame = 0;
+  
+    let loopTimeout;
+  
+    // Create Noise
+    const createNoise = () => {
+      const idata = ctx.createImageData(wWidth, wHeight);
+      const buffer32 = new Uint32Array(idata.data.buffer);
+      const len = buffer32.length;
+  
+      for (let i = 0; i < len; i++) {
+        if (Math.random() < 0.5) {
+          buffer32[i] = 0xffff0000;
+        }
+      }
+  
+      noiseData.push(idata);
+    };
+  
+    // Play Noise
+    const paintNoise = () => {
+      if (frame === 5) {
+        frame = 0;
+      } else {
+        frame++;
+      }
+  
+      ctx.putImageData(noiseData[frame], 0, 0);
+    };
+  
+    // Loop
+    const loop = () => {
+      paintNoise(frame);
+  
+      loopTimeout = window.setTimeout(() => {
+        window.requestAnimationFrame(loop);
+      }, 1000 / 25);
+    };
+  
+    // Setup
+    const setup = () => {
+      wWidth = window.innerWidth;
+      wHeight = document.body.scrollHeight;
+      canvas.width = wWidth;
+      canvas.height = wHeight;
+  
+      for (let i = 0; i < 10; i++) {
+        createNoise();
+      }
+  
+      loop();
+    };
+  
+    // Reset
+    let resizeThrottle;
+    const reset = () => {
+      window.addEventListener(
+        "resize",
+        () => {
+          window.clearTimeout(resizeThrottle);
+  
+          resizeThrottle = window.setTimeout(() => {
+            window.clearTimeout(loopTimeout);
+            setup();
+          }, 200);
+        },
+        false
+      );
+    };
+  
+    // Init
+    const init = (() => {
+      canvas = document.getElementById("noise2");
+      ctx = canvas.getContext("2d");
+  
+      setup();
+    })();
+  };
+  
+  noise2();
+  
